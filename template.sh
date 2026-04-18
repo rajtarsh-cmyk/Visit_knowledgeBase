6254fca98d21a3fe2747e119ac4fa2a09c80a06d
rajtarsh-cmyk <rajtarsh@gmail.com>
2026-04-18 23:20:59 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,18 @@
+mkdir -p src
+mkdir -p research
+
+#creating file 
+
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch README.md
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo "Directory and files created successfully!."
+
